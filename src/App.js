986ebedae960db6f735aceeca70a0833f375bce6
Erick Oliveira <ericksilvaredes@gmail.com>
2022-10-14 17:37:25 -0300

    buscando dados da API por meio do fetch()

--- src/App.js
+++ src/App.js
@@ -3,31 +3,28 @@ import { Component } from 'react'
 class App extends Component {
   constructor(){
     super(); 
-    
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: 0
-        },
-        {
-          name: 'Frank',
-          id: 1
-        },
-        {
-          name: 'Jacky',
-          id: 2
-        }
-      
-      ]
+      monsters: []
     }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => { return { monsters: users }},
+        () => { console.log(this.state)}
+      ))
   }
   render() {
     return (
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1 key={monster.id}>{monster.name}</h1>
+            return(
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
           })
         }
       </div>
