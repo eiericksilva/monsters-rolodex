0afc9498fb16c3beb628a93e0cd104b8ca5cf57b
Erick Oliveira <ericksilvaredes@gmail.com>
2022-10-14 15:02:50 -0300

    Utilizando map para percorrer array de oobjetos e renderizar jsx

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import { Component } from 'react'
 class App extends Component {
   constructor(){
     super(); 
+    
     this.state = {
-      monster1: {
-        name: 'Linda'
-      },
-      monster2: {
-        name: 'Frank'
-      },
-      monster3: {
-        name: 'Jacky'
-      }
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jacky'
+        }
+      
+      ]
     }
   }
   render() {
     return (
       <div className="App">
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
