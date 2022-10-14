d9bf29993a9fac386f84009006bdfcd65c840d97
Erick Oliveira <ericksilvaredes@gmail.com>
2022-10-14 08:26:49 -0300

    state nas classes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Component } from 'react'
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: 'Erick'
+    }
+  }
   render() {
     return (
       <div className="App">
-        <h4>
-          Teste
-        </h4>
+        <p>Name: {this.state.name}</p>
+        <button>Change Name</button>
       </div>
     );
   }
