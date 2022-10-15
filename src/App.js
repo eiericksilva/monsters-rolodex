fd105faa79d48faa68318c15dda34d145a1453d7
Erick Oliveira <ericksilvaredes@gmail.com>
2022-10-15 14:23:49 -0300

    2 otimizações

--- src/App.js
+++ src/App.js
@@ -16,9 +16,22 @@ class App extends Component {
         () => { console.log(this.state)}
       ))
   }
+  onSearchChange =  (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(
+      () => {
+      return { searchField }
+      }
+    )
+  }
+
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    const {monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
     })
     
     return (
@@ -27,14 +40,7 @@ class App extends Component {
           className='search-box'
           type='search' 
           placeholder='search monsters' 
-          onChange={ (event) => {
-            const searchField = event.target.value.toLocaleLowerCase()
-            this.setState(
-              () => {
-              return { searchField }
-              }
-            )
-          }}/>
+          onChange={onSearchChange}/>
         {
           filteredMonsters.map((monster) => {
             return(
