bc686dfb8881a0c005783446c6652c38b8b9f318
Erick Oliveira <ericksilvaredes@gmail.com>
2022-10-15 13:35:43 -0300

    colocando o array original como referência para filtros

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
     super(); 
     this.state = {
       monsters: [],
+      searchField:'' 
     }
   }
   componentDidMount() {
@@ -16,24 +17,26 @@ class App extends Component {
       ))
   }
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    })
+    
     return (
       <div className="App">
         <input
           className='search-box'
           type='search' 
           placeholder='search monsters' 
-          onChange={ event => {
-            const searchString = event.target.value.toLocaleLowerCase()
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString)
-            })
-
-            this.setState(() => {
-              return {monsters: filteredMonsters}
-            })
+          onChange={ (event) => {
+            const searchField = event.target.value.toLocaleLowerCase()
+            this.setState(
+              () => {
+              return { searchField }
+              }
+            )
           }}/>
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return(
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
