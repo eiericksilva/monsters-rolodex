34e5e9af63f9aadd1c1b39336899dd6e5f9a84c2
Erick Oliveira <ericksilvaredes@gmail.com>
2022-10-17 22:31:27 -0300

    Convertendo componentes de classe em Funcionais

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react';
+
 import './App.css'
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 
-class App extends Component {
-  constructor(){
-    super(); 
-    this.state = {
-      monsters: [],
-      searchField:'' 
-    }
-  }
-  componentDidMount() {
+const App = () => {
+  const [ searchField, setSearchField ] = useState('');
+  const [ monsters, setMonsters ] = useState([]);
+  const [ filteredMonsters, setFilteredMonsters ] = useState(monsters);
+
+  useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(
-        () => { return { monsters: users }},
-      ))
-  }
-  onSearchChange =  (event) => {
-    const searchField = event.target.value.toLocaleLowerCase()
-    this.setState(
-      () => {
-      return { searchField }
-      }
-    )
-  }
+      .then((users) => setMonsters(users))
+  },[]);
 
-
-  render() {
-    const {monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField)
-    })
-    
-    return (
-      <div className="App">
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
+  
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase()
+    setSearchField(searchFieldString)
+  }
+
+  return (
+    <div className="App">
         <h1 className='app-tittle'> Monters Rolodex</h1>
         <SearchBox 
           className='search-box'
           onChangeHandler={onSearchChange}
           placeholder='search monsters'/>
         <CardList monsters={filteredMonsters}/>
-      </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default App;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,11 @@
-import { Component } from "react";
 import "./search-box.styles.css"
 
-class SearchBox extends Component {
-    render() {
-        return (
-            <input
-                type='search' 
-                className={this.props.className}
-                placeholder={this.props.placeholder}
-                onChange={this.props.onChangeHandler}
-            />
-        )
-    }
-   
-}
+const SearchBox = ({className,placeholder,onChangeHandler}) => (
+    <input
+        type='search' 
+        className={className}
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+    />
+)
 export default SearchBox
